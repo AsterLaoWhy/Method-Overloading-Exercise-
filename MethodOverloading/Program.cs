e0400378e28a9a7316487ef523408c221d919804
Jon <jonathon_rylatt@yahoo.com>
2023-12-14 19:09:07 +0800

    Overloaded the methods

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,6 +2,20 @@
 {
     public class Program
     {
+        static int Add(int x, int y)
+        {
+            return x+y;
+        }
+        static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        static string Add(double x, double y, bool isDollar)
+        {
+            if (isDollar && x + y > 1) return $"{x + y} dollars";
+            if (isDollar && x + y <= 1) return $"{x + y} dollar";
+            return $"{x + y}";
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
